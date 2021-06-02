0950f3cc43d5a276d7906e76ebd5606bbe3262bc
Anunoy Dussa <adussa3@gmail.com>
2021-06-02 11:19:22 -0500

    Updated README and removed unused imports
    1) Updated README.md
    2) Removed unused imports in MainActivity.kt

--- app/src/main/java/com/example/android/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/android/aboutme/MainActivity.kt
@@ -5,9 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import com.example.android.aboutme.databinding.ActivityMainBinding
 
